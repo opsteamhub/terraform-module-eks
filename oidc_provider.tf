d998cb6e602cfe2dbaa1be4b295ebc240df932a3
rafael <rafaelviniciuscardoso@gmail.com>
2022-03-22 20:36:57 -0300

    add files

--- oidc_provider.tf
+++ oidc_provider.tf
@@ -15,3 +15,7 @@ data "aws_eks_cluster" "cluster" {
 data "aws_eks_cluster_auth" "cluster" {
   name = aws_eks_cluster.eks_cluster.id
 }
+
+data "tls_certificate" "main" {
+  url = data.aws_eks_cluster.cluster.identity[0].oidc[0].issuer
+}
